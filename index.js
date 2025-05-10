756d671c2023f2c5bc28824ca6a5c5611644895f
Elpidio Cabral <31358576+elpidiocabral@users.noreply.github.com>
2025-05-10 01:14:30 +0000

    deploy

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
+require("dotenv").config();
+require("./keepAlive.js");
+
 const { GatewayIntentBits, messageLink } = require("discord.js");
 const Discord = require("discord.js");
-const client = new Discord.Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
-const config = require("./config.json");
+const client = new Discord.Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages,
+    ],
+});
+const prefix = process.env.PREFIX || "%";
+const token = process.env.TOKEN;
 const fs = require("fs");
 
-client.commands = new Discord.Collection()
-const commands = fs.readdirSync("./Commands").filter(file => file.endsWith(".js"))
+client.commands = new Discord.Collection();
+const commands = fs
+    .readdirSync("./Commands")
+    .filter((file) => file.endsWith(".js"));
 // for de busca comandos na subpasta
-for(file of commands) {
-    const nameCommand = file.split(".")[0]
-    const command = require(`./Commands/${nameCommand}`)
+for (file of commands) {
+    const nameCommand = file.split(".")[0];
+    const command = require(`./Commands/${nameCommand}`);
     console.log(`Trying command: ${nameCommand}`); // testando comando requerido
-    client.commands.set(nameCommand, command)
+    client.commands.set(nameCommand, command);
 }
 
 // online console alert
-client.on('ready', () => {
+client.on("ready", () => {
     console.log(`Online in -> ${client.user.tag}!`);
-} );
+});
 
 // handler?
-client.on("messageCreate", msg => {
-    if(msg.content.startsWith(config.prefix)) {
+client.on("messageCreate", (msg) => {
+    if (msg.content.startsWith(prefix)) {
         //const info = msg.content.toLowerCase()
-        const args = msg.content.slice(config.prefix.length).trim().split(/ +/g)
-        const nameCommand = args.shift()
-        const command = client.commands.get(nameCommand)
+        const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+        const nameCommand = args.shift();
+        const command = client.commands.get(nameCommand);
         //console.log(`${args.slice(0)}`)
-        if(!command) return
-        command.run(client, msg, args) 
+        if (!command) return;
+        command.run(client, msg, args);
     }
-} )
+});
 
 // bot on
-client.login(config.token)
+client.login(token);
