55b5f9bc27e0790b307c19d745aa5a04a0f59b1c
Elpidio Cabral <netoelpidio1@gmail.com>
2025-05-09 21:39:07 -0300

    alteração-das-pastas

---
+++ Commands/sistema.js
@@ -0,0 +1,5 @@
+const { messageLink } = require("discord.js")
+
+module.exports.run = (client, msg, args) => {
+    msg.reply("Link para o Sistema A Vontade dos Mares: ***https://homebrewery.naturalcrit.com/share/nj8OcL4278Sn***")
+}
